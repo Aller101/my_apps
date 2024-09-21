38fbb620acf07ff888d05fc70174241d48c1423c
Aleksey Nesterov <zubatka.forever@icloud.com>
2024-09-21 12:25:33 +0400

    new project

---
+++ service-organiser/src/main/java/com/springboot/restservice/service_organiser/ServiceOrganiserApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restservice.service_organiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrganiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOrganiserApplication.class, args);
+	}
+
+}
