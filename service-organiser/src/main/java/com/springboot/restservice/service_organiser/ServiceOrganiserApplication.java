4f8c811de09a894bdd039a5cd71cda4924877f13
Aleksey Nesterov <zubatka.forever@icloud.com>
2024-09-22 20:59:16 +0400

    fixed bug: spring boot embedded database (H2, HSQL or Derby), put it on the class path error resolved

--- service-organiser/src/main/java/com/springboot/restservice/service_organiser/ServiceOrganiserApplication.java
+++ service-organiser/src/main/java/com/springboot/restservice/service_organiser/ServiceOrganiserApplication.java
@@ -1,9 +1,16 @@
 package com.springboot.restservice.service_organiser;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//была ошибка:
+//Consider the following: If you want an embedded database (H2, HSQL or Derby),
+//please put it on the classpath. If you have database settings to be loaded from
+//a particular profile you may need to activate it (no profiles are currently active).
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ServiceOrganiserApplication {
 
 	public static void main(String[] args) {
